e1fc64280deef6c9bf99088d780862c939930b63
jony2511 <141722597+jony2511@users.noreply.github.com>
2025-09-01 11:24:55 +0600

    Login page added (to change someting login needed)

---
+++ login.php
@@ -0,0 +1,9 @@
+<?php ?><!DOCTYPE html>
+<html><head><meta charset="utf-8"><title>Login</title></head>
+<body>
+  <form method="post" action="process.php">
+    <input name="username" placeholder="Username">
+    <input name="password" type="password" placeholder="Password">
+    <button type="submit" name="action" value="login">Login</button>
+  </form>
+</body></html>
