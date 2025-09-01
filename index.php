f5f49d02d8800568a1bd5976199b5e76188f8e9e
jony2511 <141722597+jony2511@users.noreply.github.com>
2025-09-01 11:10:16 +0600

    Basic index.php setup

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-<?php echo 'Hello from my portfolio'; ?>
+<?php
+?><!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Portfolio</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <header><h1>HI! I am Jony</h1></header>
+  <main>
+    <p>Welcome to my portfolio.</p>
+  </main>
+</body>
+</html>
